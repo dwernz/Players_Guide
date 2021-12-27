cd4969e3a6867e909335756f49269a9bd72309db
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-27 12:09:09 -0500

    Streamlined

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace Players_Guide
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿System.Console.WriteLine("Hello World!");
