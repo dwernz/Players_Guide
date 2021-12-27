b54d427f01264b35a707d9c1405a59f4285a2b17
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-27 13:16:54 -0500

    Challenge: The Thing Namer 3000

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 
-string name;
-
-Console.WriteLine("Bread is ready.");
-Console.WriteLine("Who is the bread for?");
-name = Console.ReadLine();
-Console.WriteLine("Noted: " + name + " got bread");
+Console.WriteLine("What kind of thing are we talking about?");
+// Name of object
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// Description of the object
+string b = Console.ReadLine();
+// String Doom
+string c = "Doom";
+// String version
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
