b7f56a352dda7dc3be1748e1cb226466bda0b941
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-27 12:18:11 -0500

    Variables

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-Console.WriteLine("Welcome to The Realms of C#");
-Console.WriteLine("Ritlin greets his friends");
-Console.WriteLine("They talk");
-Console.WriteLine("His friends stare at you in amazement");
-Console.WriteLine("You take up the challenge");
+
+string name;
+name = "User";
+Console.WriteLine("Hi " + name);
