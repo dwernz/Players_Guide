9e6404319b4d1d77eeaffda2a529383d709147e4
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-27 12:16:29 -0500

    Challenge: The Makings of a Programmer

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -1,2 +1,6 @@
 ﻿using System;
-Console.WriteLine("Hello World!");
+Console.WriteLine("Welcome to The Realms of C#");
+Console.WriteLine("Ritlin greets his friends");
+Console.WriteLine("They talk");
+Console.WriteLine("His friends stare at you in amazement");
+Console.WriteLine("You take up the challenge");
