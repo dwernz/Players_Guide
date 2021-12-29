d996799182e1379e0503c52908e62e0a86c8cbd4
Daniel <62781672+dwernz@users.noreply.github.com>
2021-12-28 19:09:16 -0500

    Console upgrade for The Dominion of Kings

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -2,12 +2,22 @@
 
 // Challenge: The Dominion of Kings
 
+Console.Title = "The Dominion of Kings";
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.Clear();
+
 Console.Write("How many provinces does the King rule? ");
 string provincesString = Console.ReadLine();
+Console.Clear();
 Console.Write("How many duchies does the King rule? ");
 string duchiesString = Console.ReadLine();
+Console.Clear();
 Console.Write("How many estates does the King rule? ");
 string estatesString = Console.ReadLine();
+Console.Clear();
 
 int provinces = Convert.ToInt32(provincesString);
 int duchies = Convert.ToInt32(duchiesString);
@@ -19,4 +29,4 @@ int provinceTotal = provinces * 6;
 
 int total = estateTotal + duchiesTotal + provinceTotal;
 
-Console.WriteLine("Total points for the King is " + total);
+Console.WriteLine($"Total points for the King is {total}");
