575a7f76c2b5a562d147ad527484172dbfc96670
Daniel <62781672+dwernz@users.noreply.github.com>
2022-01-03 11:57:11 -0500

    Challenge: The Replicator of D'To

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -1,36 +1,38 @@
 ﻿using System;
 
-// Challenge: The Prototype
+// Challenge: The Replicator of D'To
 
-Console.Title = "The Prototype";
+Console.Title = "The Replicator of D'To";
 
 Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.White;
 
 Console.Clear();
 
-for (int counter = 1; counter <= 100; counter++)
+int[] array1 = new int[5];
+
+for (int counter = 1; counter <= 5; counter++)
 {
-    if (counter % 3 == 0 && counter % 5 == 0)
-    {
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine(counter + ": Fire/Electric");
-    }
-    else if (counter % 3 == 0)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(counter + ": Fire");
-    }
-    else if (counter % 5 == 0)
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(counter + ": Electric");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine(counter + ": Normal");
-    }
+    Console.Write("Enter a number to be stored in the array: ");
+    array1[counter - 1] = Convert.ToInt32(Console.ReadLine()); 
 }
 
-Console.ForegroundColor = ConsoleColor.White;
+int[] array2 = new int[5];
+
+for (int counter = 0; counter <= 4; counter++)
+{
+    array2[counter] = array1[counter];
+}
+
+Console.Write("Array1: ");
+
+foreach (int i in array1)
+{
+    Console.Write("{0} ", i);
+}
+
+Console.Write("\nArray2: ");
+foreach (int i in array2)
+{
+    Console.Write("{0} ", i);
+}
