643a41ff8d785350ce60d195954d32ade3c2fe7b
Daniel <62781672+dwernz@users.noreply.github.com>
2022-01-05 15:50:38 -0500

    Challenge: Simula's Test

--- Players_Guide/Program.cs
+++ Players_Guide/Program.cs
@@ -1,117 +1,49 @@
 ﻿using System;
 
-// Boss Battle: Hunting the Manticore
+// Challenge: Simula's Test
+
+// Define an enumeration for the state of the chest.
+// Make a variable whose type is this new enumeration.
+// Write code to allow you to manipulate the chest with the lock, unlock, open, and close commands, but ensure that you don't transition between states that don't support it.
+// Loop forever, asking fo the next command
 
 
 // Set Console attributes
-Console.Title = "Hunting the Manticore";
+Console.Title = "Simula's Test";
 Console.BackgroundColor = ConsoleColor.Black;
 Console.ForegroundColor = ConsoleColor.White;
 
 Console.Clear();
 
+ChestState currentState = ChestState.Locked;
 
-bool isManticoreDefeated = false;
-bool isCityDestroyed = false;
+Console.WriteLine("Commands: unlock, open, close, lock\n");
 
-int round = 1;
-int cityHealth = 15;
-int manticoreHealth = 10;
-int cannonDamage = 1;
-int aim = 0;
-
-Console.WriteLine("Player 1, how far away from the city do you want to station the Manticore? (1-100)");
-int manticoreDistance = Convert.ToInt32(Console.ReadLine());
-
-while (manticoreDistance > 100 || manticoreDistance < 1)
+while (true)
 {
-    Console.Write("Invalid range, please enter a number between 1 and 100: ");
-    manticoreDistance = Convert.ToInt32(Console.ReadLine());
-    Console.Clear();
+    Console.Write($"The chest is {currentState}. What do you want to do? ");
+
+    string input = Console.ReadLine();
+
+    if (currentState == ChestState.Locked && input == "unlock")
+    {
+        currentState = ChestState.Closed;
+    }
+
+    if (currentState == ChestState.Closed && input == "open")
+    {
+        currentState = ChestState.Open;
+    }
+
+    if (currentState == ChestState.Closed && input == "lock")
+    {
+        currentState = ChestState.Locked;
+    }
+
+    if (currentState == ChestState.Open && input == "close")
+    {
+        currentState = ChestState.Closed;
+    }
 }
 
-Console.Clear();
-
-Console.WriteLine("Player 2, it is your turn");
-
-
-
-while (!isManticoreDefeated && !isCityDestroyed) 
-{
-    DisplayStatus(); 
-}
-
-if (isManticoreDefeated)
-{
-    Console.WriteLine("The Manticore has been destroyed! The city of Consolas has been saved!");
-}
-
-if (isCityDestroyed)
-{
-    Console.WriteLine("You failed to stop the Manticore. The city of Consolas has been destroyed");
-}
-
-void DisplayStatus()
-{
-    if (round % 3 == 0 && round % 5 == 0)
-    {
-        cannonDamage = 10;
-    }
-    else if (round % 3 == 0)
-    {
-        cannonDamage = 3;
-    }
-    else if (round % 5 == 0)
-    {
-        cannonDamage = 5;
-    }
-    else
-    {
-        cannonDamage = 1;
-    }
-    
-    Console.WriteLine("STATUS: Round: " + round + " City: " + cityHealth + "/15 Manticore: " + manticoreHealth + "/10");
-    Console.WriteLine("The cannon is expected to deal " + cannonDamage + " damage this round.");
-    Console.Write("Enter the desired cannon range: ");
-    aim = Convert.ToInt32(Console.ReadLine());
-
-    if (aim == manticoreDistance)
-    {
-        manticoreHealth -= cannonDamage;
-        Console.WriteLine("That round was a DIRECT HIT!");
-        Console.WriteLine();
-        if (manticoreHealth <= 0)
-        {
-            isManticoreDefeated = true;
-            return;
-        }
-
-        manticoreDistance = 0;
-        
-        Console.Write("Player 1, move the Manticore! (1-100): ");
-        while (manticoreDistance > 100 || manticoreDistance < 1)
-        {
-            Console.Write("Invalid range, please enter a number between 1 and 100: "); ;
-            manticoreDistance = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-        }
-    }
-    else if (aim > manticoreDistance)
-    {
-        Console.WriteLine("That round OVERSHOT the target.\n\n");
-    }
-    else if (aim < manticoreDistance)
-    {
-        Console.WriteLine("That round FELL SHORT of the target.\n\n");
-    }
-
-    cityHealth--;
-
-    if (cityHealth <= 0)
-    {
-        isCityDestroyed = true;
-        return;
-    }
-
-    round++;
-}
+enum ChestState { Open, Closed, Locked};
